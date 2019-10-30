4adfe75462b0cc7149a1f2f56d67ffd1d5e28fb6
Ferran-Roger Basart i Bosch <47557484+ferba93@users.noreply.github.com>
2019-10-30 21:42:46 +0100

    Debug functionality

--- Pinball/ModulePlayer.cpp
+++ Pinball/ModulePlayer.cpp
@@ -2,6 +2,7 @@
 #include "ModuleSceneIntro.h"
 #include "Application.h"
 #include "ModulePlayer.h"
+#include "ModuleInput.h"
 
 ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -32,12 +33,27 @@ update_status ModulePlayer::Update()
 }
 
 update_status ModulePlayer::PreUpdate() {
+	
+	// Stage calculation
 	if (position.y < 0 && App->scene_intro->stage == ST_LOW_STAGE) {
 		App->scene_intro->stage == ST_HIGH_STAGE;
 	}
 	if (position.y > SCREEN_HEIGHT && App->scene_intro->stage == ST_HIGH_STAGE) {
 		App->scene_intro->stage == ST_LOW_STAGE;
 	}
+	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN && App->scene_intro->stage == ST_TITLE_SCREEN) {
+		App->scene_intro->stage == ST_LOW_STAGE;
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_DOWN) {
+
+	}	// LEFT FLIPPER
+	else if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_DOWN) {
+
+	}	// RIGHT FLIPPER
+	else if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_DOWN || App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT) {
+
+	}	// KICKER
 	return UPDATE_CONTINUE;
 }
 
